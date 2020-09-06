852e002ede70a38b1124945e00c7c965feb17aee
ABid A.Sobhan <abida.sobhan@ABids-iMac.local>
2020-09-06 01:55:25 -0700

    Initial Commit

---
+++ Sultan Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sultan Tracker
+//
+//  Created by ABid A.Sobhan on 6/9/20.
+//  Copyright © 2020 ABid A.Sobhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
